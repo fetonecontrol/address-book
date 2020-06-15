9d49168ce79abecaff5d36a8aecd2e3d6906518b
Frederick Ernest <frederick.ernest@gmail.com>
2020-06-15 11:59:05 -0700

    create working appendix function in html output
    co-authored-by Jason Khan <jasonderekkhan@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -18,4 +18,33 @@ function Contact(firstName, lastName, location, phoneNumber) {
 Contact.prototype.fullName = function() {
   return this.firstName + " " + this.lastName;
 }
+
+
+
+function list(array){
+
+  for (let i=0; i <=array.length-1; i++) {
+    addressBook.contacts[i].fullName;
+    return array1;
+  }
+}
+
 // User Interface Logic ---------
+
+$(document).ready(function() {
+  $("form#form1").submit(function(event) {
+    event.preventDefault();
+//Begin User Input ---------
+    let firstName = $("#firstName").val();
+    let lastName = $("#lastName").val();
+    let location = $("#location").val();
+    let number = $("#number").val();
+//End User Input ----------
+    let addressBook = new AddressBook();
+    let contact1 = new Contact(firstName, lastName, location, number);
+    addressBook.addContact(contact1);
+    console.log(contact1);
+//    let output = list(addressBook.contacts[0].firstName);
+    $("ol#result").append("<li>" + addressBook.contacts[0].firstName + "</li>");
+  });
+});
